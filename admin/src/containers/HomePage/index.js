e729e24cd6034bd2cd5db63d32df55f765b230b1
Daniel Hoffmann Bernardes <daniel.hoffmann.bernardes@gmail.com>
2020-12-02 10:26:57 +0100

    wip

---
+++ admin/src/containers/HomePage/index.js
@@ -0,0 +1,13 @@
+/*
+ *
+ * HomePage
+ *
+ */
+/* eslint-disable */
+import React, { memo } from "react";
+
+const HomePage = ({ history: { push } }) => {
+  return <h1>Welcome to Fault Code CMS</h1>;
+};
+
+export default memo(HomePage);
